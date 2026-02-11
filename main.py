0c9b3c391e6b9ca209223cae25b87e106bae3aba
1 <inside12111@gmail.com>
2026-02-11 18:20:05 +0300

    Финал

--- main.py
+++
@@ -1,7 +0,0 @@
-import sqlite3, pandas as pd
-
-df = pd.read_csv('data/raw/legal_full_2026-01-20_23-28-59.csv')
-
-ds = sqlite3.connect('output/legal_firms.db')
-
-df.to_sql('firms', ds, if_exists='replace', index=False)
